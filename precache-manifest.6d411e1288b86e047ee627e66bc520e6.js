fe17d46d2e670ded994c7d8a753677ca11fbe196
Tatyana Pivgorodnyaya <peachpeachttt@gmail.com>
2020-05-13 13:51:22 +0300

    Updates

---
+++ precache-manifest.6d411e1288b86e047ee627e66bc520e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ec2cfbdce4eecb7b8635febbba2582",
+    "url": "/PhoneBook/index.html"
+  },
+  {
+    "revision": "abf8986b6020c59bbb02",
+    "url": "/PhoneBook/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5da8b28cbd1ee8133312",
+    "url": "/PhoneBook/static/css/main.68ea188d.chunk.css"
+  },
+  {
+    "revision": "abf8986b6020c59bbb02",
+    "url": "/PhoneBook/static/js/2.4a8ae733.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PhoneBook/static/js/2.4a8ae733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da8b28cbd1ee8133312",
+    "url": "/PhoneBook/static/js/main.83b1cb1d.chunk.js"
+  },
+  {
+    "revision": "72d2ce84781eae6176cb",
+    "url": "/PhoneBook/static/js/runtime-main.bd5d684e.js"
+  }
+]);
